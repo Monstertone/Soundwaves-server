9da8ea7bc9cb5166c6090dd6fa4b4f37a142086c
Thomas Gabauer <tgabauer@gmail.com>
2018-12-20 15:44:39 -0700

    added notNullable to several other user fields

--- db/migrations/20181220143140_user.js
+++ db/migrations/20181220143140_user.js
@@ -2,13 +2,16 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('user', (table)=>{
     table.increments();
-    table.string('username');
+    table.string('username')
+         .notNullable();
     table.string('user_type');
     table.string('email')
          .unique()
          .notNullable();
-    table.string('password');
-    table.date('birth_date');
+    table.string('password')
+         .notNullable();
+    table.date('birth_date')
+         .notNullable();
     table.string('gender');
     table.binary('share_info');
     table.timestamps(true, true);
