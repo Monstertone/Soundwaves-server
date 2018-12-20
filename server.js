6b357e3ef78641a7127ef019724e6c00a333a48b
Thomas Gabauer <tgabauer@gmail.com>
2018-12-20 14:23:51 -0700

    server now working.  Added cors, altered package.json to remove index.js

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require("path");
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
+
 
 var routes_setter = require('./config/routes.js');
 routes_setter(app);
